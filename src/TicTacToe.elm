c1fc121285634411d965771538a16503bf4f61fd
Chandan Kumar <cksharma122@gmail.com>
2020-02-16 01:12:45 +0530

    Add onClicks

--- src/TicTacToe.elm
+++ src/TicTacToe.elm
@@ -2,15 +2,14 @@ module TicTacToe exposing (main)
 
 import Browser exposing (sandbox)
 import Html exposing (Html, button, div, input, text)
-import Html.Attributes exposing (value)
+import Html.Attributes exposing (class, value, style)
 import Html.Events exposing (onClick, onInput)
 import List exposing (..)
 
 main = sandbox {init = init, view = view, update = update }
 
 type Msg
-    = UpdateTurn
-    | MakeMove
+    = PlaceSymbol Int 
 
 type Symbol 
     = X
@@ -18,26 +17,48 @@ type Symbol
     | Empty
     
 type alias Model = 
-    { board : List Symbol
+    {board : List Symbol
+    ,currentSymbol : Symbol
     }
 
 init : Model
-init = { board = [Empty, Empty, Empty,Empty, Empty, Empty,Empty, Empty, Empty] }
+init = { board = [Empty, Empty, Empty,Empty, Empty, Empty,Empty, Empty, Empty] 
+        ,currentSymbol = X
+        }
+
+updateTurn : Symbol -> Symbol
+updateTurn s =
+    if s == X
+    then O
+    else X
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
-        UpdateTurn ->
-            init
+        PlaceSymbol position ->
+            {board = updateInList position model.board model.currentSymbol
+            ,currentSymbol = updateTurn model.currentSymbol
+            }
 
-        MakeMove ->
-            init
-
-cell : Symbol -> Html Msg
-cell symbol =
-    button [ onClick MakeMove ] [ text "X" ]
+cell : Int -> Symbol -> Html Msg
+cell position symbol =
+    button (if symbol == Empty 
+            then [onClick (PlaceSymbol position)] 
+            else []) [ text (Debug.toString symbol) ]
 
 view : Model -> Html Msg
 view model =
-    div []
-      (map cell model.board)
+    div [style "display" "grid"
+        ,style "grid-template-columns" "auto auto auto"
+        ,style "width" "150px"]
+      (indexedMap cell model.board)
+
+setNewVal : Int -> Symbol -> Int -> Symbol -> Symbol
+setNewVal p ns i cs =
+    if i == p && cs == Empty
+    then ns
+    else cs
+
+updateInList : Int -> List Symbol -> Symbol -> List Symbol
+updateInList pos board ns =
+    indexedMap (setNewVal pos ns) board
